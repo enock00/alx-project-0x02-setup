12fac8d6c44112dc14d22213348a7f1d3dbe4b5e
Enock <enockzy54@gmail.com>
2025-10-04 22:55:35 +0300

    Basic Routing in Next.js

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -6,7 +6,7 @@ const Header: React.FC = () => {
     <header className="bg-blue-600 text-white p-4 shadow-md flex gap-6">
       <h1 className="text-xl font-bold">ALX Project 2</h1>
       <nav className="flex gap-4">
-        <Link href="/">Home</Link>
+        <Link href="/home">Home</Link>
         <Link href="/about">about</Link>
       </nav>
     </header>
