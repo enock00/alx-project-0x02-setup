57516a02458c9ee2803ed89a926497c0c3abf53c
Enock <enockzy54@gmail.com>
2025-10-05 13:52:13 +0300

    Created a Reusable Card Component

--- alx-project-0x02/components/common/Card.tsx
+++ alx-project-0x02/components/common/Card.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { CardProps } from "@/interfaces";
+import { type CardProps } from "@/interfaces";
 
 const Card: React.FC<CardProps> = ({ title, content }) => {
   return (
-    <div className="bg-white shadow-md rounded-xl p-8 border hover:shadow-lg transition">
+    <div className="bg-white shadow-md rounded-xl p-6 border hover:shadow-lg transition">
       <h2 className="text-xl font-semibold text-gray-800 mb-2">{title}</h2>
       <p className="text-gray-600">{content}</p>
     </div>
@@ -11,3 +11,4 @@ const Card: React.FC<CardProps> = ({ title, content }) => {
 };
 
 export default Card;
+
